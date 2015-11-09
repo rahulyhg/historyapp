8afbf0d24dd834b547511b44432798aa09a809e8
Raphael Rocha <raphaelrocha86@gmail.com>
2015-11-09 19:12:22 -0400

    Primeiro commit

---
+++ server.php
@@ -0,0 +1,100 @@
+<?php 
+header ('Content-type: text/html; charset=UTF-8');
+
+/*
+* @ localhost = 0;
+* @ ica = 1;
+* @ hostinger = 2;
+*/
+$serverLocation = 0;
+
+$host;
+$db;
+$user;
+$passwd;
+
+if($serverLocation == 0){
+	$host = 'localhost';
+	$db = 'history_app';
+	$user = 'root';
+	$passwd = '';
+
+} else if($serverLocation == 1){
+	$host = 'localhost';
+	$db = 'quebragalho';
+	$user = 'quebragalho_user';
+	$passwd = '@#RJQUEBRAgalho@#UFAM@#2015';
+} else if($serverLocation == 2){
+	$host = 'mysql.hostinger.com.br';
+	$db = 'u992178256_qg';
+	$user = 'u992178256_qg';
+	$passwd = 'shellscript';
+}
+
+$conn = new mysqli($host,$user,$passwd,$db);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+if (!$conn->set_charset("utf8")) {
+    printf("Error loading character set utf8: %s\n", $conn->error);
+}
+
+function pop_user($model){
+	$array_user = array(
+			"id"=>$model["id"],
+			"name"=>$model["name"],
+			"email"=>$model["email"],
+			"picture_profile"=>$model["picture_profile"]
+			);
+	return $array_user;
+}
+
+function pop_place($model){
+	$array_place = array(
+			"id"=>$model["id"],
+			"id_district"=>$model["id_district"],
+			"name"=>$model["name"],
+			"addr"=>$model["addr"],
+			"location"=>$model["location"],
+			"picture_place"=>$model["picture_place"]
+			);
+	return $array_place;
+}
+
+
+
+
+/*
+
+if(isset($_POST['method'])){
+
+	if(strcmp('object', $_POST['method']) == 0){
+		$method = $_POST['method'];
+		$dados = $_POST['data'];
+		$mensagem_para_enviar = "MEDOTO: ".$method." | DADOS: ".$dados;
+		echo json_encode(array('mensagem'=>$mensagem_para_enviar));
+	}
+
+	else if(strcmp('array', $_POST['method']) == 0){
+		$method = $_POST['method'];
+		$dados = $_POST['data'];
+		$mensagem_para_enviar = "METODO: ".$method." | DADOS: ".$dados;
+
+		$array = array();
+		array_push($array, array('mensagem'=>$mensagem_para_enviar)); 
+		
+		$array_final = array();
+		array_push($array_final, $array);
+		echo json_encode($array);
+	}
+
+	else{
+		echo "erro no methos";
+	}
+
+}else{
+	echo "erro geral";
+}
+
+ ?>*/
