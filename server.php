7ad863a557bfc39b53a2a76507cd1407a6d309a1
Raphael Rocha <raphaelrocha86@gmail.com>
2015-11-18 22:24:12 -0400

    primeiras funções

--- server.php
+++ server.php
@@ -21,11 +21,12 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 if (!$conn->set_charset("utf8")) {
-    printf("Error loading character set utf8: %s\n", $conn->error);
+    print_r("Error loading character set utf8: %s\n", $conn->error);
 }
 
-function pop_user($model){
+/*function pop_user($model){
 	$array_user = array(
+			"response"=>null,
 			"id"=>$model["id"],
 			"name"=>$model["name"],
 			"email"=>$model["email"],
@@ -36,6 +37,7 @@ function pop_user($model){
 
 function pop_place($model){
 	$array_place = array(
+		"response"=>null,
 			"id"=>$model["id"],
 			"id_district"=>$model["id_district"],
 			"name"=>$model["name"],
@@ -44,41 +46,141 @@ function pop_place($model){
 			"picture_place"=>$model["picture_place"]
 			);
 	return $array_place;
-}
-
-
-
-
-/*
+}*/
 
 if(isset($_POST['method'])){
 
-	if(strcmp('object', $_POST['method']) == 0){
-		$method = $_POST['method'];
+	//retorna json array
+	if(strcmp('list_cats', $_POST['method']) == 0){
+
 		$dados = $_POST['data'];
-		$mensagem_para_enviar = "MEDOTO: ".$method." | DADOS: ".$dados;
-		echo json_encode(array('mensagem'=>$mensagem_para_enviar));
+		$array_response = array();
+
+		$sql = "select * from category";
+
+		$result = $conn->query($sql);
+
+		if($result!=null){
+			if($result->num_rows > 0){
+				foreach ($result as $model) {
+					array_push($array_response,$model);
+				}
+			}else{
+				array_push($array_response,array("id"=>"not_found"));
+			}
+		}else{
+			array_push($array_response,array("id"=>"not_found"));
+		}
+
+		echo json_encode($array_response);
 	}
 
-	else if(strcmp('array', $_POST['method']) == 0){
-		$method = $_POST['method'];
-		$dados = $_POST['data'];
-		$mensagem_para_enviar = "METODO: ".$method." | DADOS: ".$dados;
-
-		$array = array();
-		array_push($array, array('mensagem'=>$mensagem_para_enviar)); 
+	//retorna json array
+	else if(strcmp('list_places_by_cats', $_POST['method']) == 0){
 		
-		$array_final = array();
-		array_push($array_final, $array);
-		echo json_encode($array);
+		$id_cat = $_POST['data'];
+		$array_response = array();
+
+		$sql = "select * from place where id_category = $id_cat";
+
+		$result = $conn->query($sql);
+
+		if($result!=null){
+			if($result->num_rows > 0){
+				foreach ($result as $model) {
+					array_push($array_response,$model);
+				}
+			}else{
+				array_push($array_response,array("id"=>"not_found"));
+			}	
+		}else{
+			array_push($array_response,array("id"=>"not_found"));
+		}
+		
+		echo json_encode($array_response);
 	}
 
-	else{
-		echo "erro no methos";
+	//retorna json object
+	else if(strcmp('get_place_by_id', $_POST['method']) == 0){
+		
+		$id_place = $_POST['data'];
+		$array_response = array();
+
+		$sql = "select * from place where id = $id_place";
+
+		$result = $conn->query($sql);
+
+		if($result!=null){
+			if($result->num_rows > 0){
+				foreach ($result as $model) {
+					array_push($array_response,$model);
+				}
+			}else{
+				array_push($array_response,array("id"=>"not_found"));
+			}	
+		}else{
+			array_push($array_response,array("id"=>"not_found"));
+		}
+		
+		echo json_encode($array_response);
+	}
+
+	//retorna json object
+	else if(strcmp('find', $_POST['method']) == 0){
+		
+		$query = $_POST['data'];
+		$array_response = array();
+
+		$sql = "select *, place.id as id_place
+						, place.name as name_place
+						, district.name as name_district
+				        , city.name as name_city
+				        , country.name as name_country
+				        from place join district on (place.id_district = district.id) 
+									join city on (district.id_city = city.id)
+				                    join state on (city.id_state = state.id)
+				                    join country on (state.id_country = country.id)
+						where place.name like '%$query%' or
+								place.description like '%$query%' or
+				                place.addr like '%$query%' or
+				                place.name like '%$query%' or
+				                district.name like '%$query%' or
+				                city.name like '%$query%' or
+				                state.name like '%$query%' or
+				                country.name like '%$query%';";
+
+		$result = $conn->query($sql);
+
+		if($result!=null){
+			if($result->num_rows > 0){
+				foreach ($result as $model) {
+					array_push($array_response,$model);
+				}
+			}else{
+				array_push($array_response,array("id"=>"not_found"));
+			}	
+		}else{
+			array_push($array_response,array("id"=>"not_found"));
+		}
+		
+		echo json_encode($array_response);
+	}
+
+	else{		
+		$array_response = array("response"=>"metodo_desconhecido");
+		echo json_encode($array_response);
 	}
 
 }else{
-	echo "erro geral";
+	
+	//RETORNA JSONOBJECT
+	echo "PORRA";
+	$array_response = array("response"=>"erro_geral");
+	echo json_encode($array_response);
 }
 
- ?>*/
+
+ ?>
+
+
+ 
---
+++ ~.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset = "UTF-8" />
+	<title>Quebra-Galho</title> 
+</head> 
+	<body>
+		<div id="contact-form">
+			<form id="contact" method="POST" action="auth.php">
+				URL <input type="text" name="url" id="url" size="100"/> <br/> <br/>
+				METHOD <input type="text" name="method" id="method" size="100"/> <br/> <br/>
+				DATA <input type="text" name="data" id="data" size="100"/> <br/> <br/>
+				<input type="submit" name="submit" id="submit" value="send" >
+			</form>
+		</div>
+	</body>
+</html>
