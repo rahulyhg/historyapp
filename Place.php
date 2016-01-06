431a9e94042135a214d8188c2c620d05172321c1
Raphael Rocha <raphaelrocha86@gmail.com>
2016-01-05 20:40:09 -0400

    Novos campos de latitude e longitude.

--- Place.php
+++ Place.php
@@ -1,7 +1,7 @@
 <?php
 class Place{
 	function get(){
-		$stmt = DB::getConn()->query("SELECT `id`,`id_district`,`id_category`,`name`,`description`,`addr`,`location`,`picture_place` FROM place");
+		$stmt = DB::getConn()->query("SELECT `id`,`id_district`,`id_category`,`name`,`description`,`addr`,`latitude`,`longitude`,`picture_place` FROM place");
 		$places = $stmt->fetchAll(PDO::FETCH_OBJ);
 		return $places;
 	}
@@ -79,12 +79,13 @@ class Place{
 	}
 	function update($place){
 		$conn = DB::getConn();
-		$stmt = $conn->prepare("UPDATE place SET name = :name,description = :description,addr = :addr,location = :location,picture_place = :picture_place WHERE id = :id");
+		$stmt = $conn->prepare("UPDATE place SET name = :name,description = :description,addr = :addr,latitude = :latitude,longitude = :longitude,picture_place = :picture_place WHERE id = :id");
 		$stmt->bindParam("id",$place->id);
 		$stmt->bindParam("name",$place->name);
 		$stmt->bindParam("description",$place->description);
 		$stmt->bindParam("addr",$place->addr);
-		$stmt->bindParam("location",$place->location);
+		$stmt->bindParam("latitude",$place->latitude);
+		$stmt->bindParam("longitude",$place->longitude);
 		$stmt->bindParam("picture_place",$place->picture_place);
 		$stmt->execute();
 	}
